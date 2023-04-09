0f8b9f374c0a544c5581f7629b332a80a69381fd
Nicoo <nicoo.bernstein@gmail.com>
2023-04-09 15:25:43 -0300

    Feat: mass delete function, checkbox and footer

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,23 @@
 import { BrowserRouter, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import Addform from './AddNav';
-import Productlist from './productlist';
+import Productlist from './ProductList';
 
 export default function Navbar() {
   const [enabled, setEnabled] = useState(true);
   const location = useLocation();
   const navigate = useNavigate();
+
+function massdelete(){
+var queryall = document.querySelectorAll(".delete-checkbox")
+queryall.forEach(el => {
+  console.log(el.parentNode);
+  el.parentNode.remove();
+})
+}
+
+
+
 
   const handleAddClick = () => {
     setEnabled(false);
@@ -18,7 +29,8 @@ export default function Navbar() {
     <p className="h1">Product list </p>
     <div className='navbar-nav ms-auto' style={{ marginRight: 2 + 'em', paddingLeft: 5 + "rem" }}>
         <Link className='nav-link text-light text-black border border-dark' style={{ marginRight: 1 + 'em' }} onClick={handleAddClick} disabled={!enabled} to='/addproduct'>Add</Link>
-      <button className='nav-link text-light text-black border border-dark'>Mass Delete</button>
+      <button className='nav-link text-light text-black border border-dark' id='delete-product-btn' onClick={massdelete}>Mass Delete</button>
+<div></div>
     </div>
   </nav>
   );
