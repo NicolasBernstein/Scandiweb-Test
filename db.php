544c9e81aefbd11e85e9b2a6d1d2a03a6a58067b
Nicoo <nicoo.bernstein@gmail.com>
2023-04-16 19:15:38 -0300

    Feat: Connecting database to production

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 class dbConnect{
 public $con; 
-private $server = 'localhost';
-private $dbname = 'productdb';
-private $user = 'root';
-private $pass = '';
+private $server = 'sql111.epizy.com';
+private $dbname = 'epiz_34024334_products';
+private $user = 'epiz_34024334';
+private $pass = '5NhZU9PpBaRQT';
 public function connect(){
     $this->con = mysqli_connect($this->server, $this->user, $this->pass, $this->dbname);
 }
