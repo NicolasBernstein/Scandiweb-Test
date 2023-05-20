3d55ac203898f4005523909000a16d7b41718f6b
Nicoo <nicoo.bernstein@gmail.com>
2023-05-19 23:09:53 -0300

    Feat: Create, getproducts, getsku endpoints

--- factory.php
+++ factory.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'index.php';
 interface Factory
 {
     public function CreateProduct(array $data);
@@ -8,6 +9,14 @@ class DVDFactory implements Factory
 {
     public function CreateProduct(array $data)
     {
+        echo "hi";
+        $product = new DVD($data['sku'], $data['nombre'], $data['precio'], $data['type'], $data['size'], $data["weight"], $data['height'], $data['width'], $data['length']);
+        $jsonData = json_encode($product);
+        $product->Addindb($product);
+        echo $jsonData;
+        return $product;
+        // $jsonData = json_encode($product);
+
     }
 }
 
@@ -15,11 +24,23 @@ class BookFactory implements Factory
 {
     public function CreateProduct(array $data)
     {
+        echo "hi";
+        $product = new DVD($data['sku'], $data['nombre'], $data['precio'], $data['type'], $data['size'], $data["weight"], $data['height'], $data['width'], $data['length']);
+        $jsonData = json_encode($product);
+        $product->Addindb($product);
+        echo $jsonData;
+        return $product;
     }
 }
 class FurnitureFactory implements Factory
 {
     public function CreateProduct(array $data)
     {
+        echo "hi";
+        $product = new DVD($data['sku'], $data['nombre'], $data['precio'], $data['type'], $data['size'], $data["weight"], $data['height'], $data['width'], $data['length']);
+        $jsonData = json_encode($product);
+        $product->Addindb($product);
+        echo $jsonData;
+        return $product;
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 require_once 'db.php';
 require_once 'factory.php';
+const FACTORIES = [
+    'DVD' => new DVDFactory(),
+    'BOOK' => new BookFactory(),
+    'FURNITURE' => new FurnitureFactory()
+];
 abstract class Product
 {
     // basic
-    protected $sku;
+    public $sku;
     protected $name;
     protected $price;
     protected $type;
@@ -31,6 +36,59 @@ abstract class Product
         $this->length = $length;
     }
 
+
+    public static function loadFrontendProductData(array $data): Product
+    {
+        $type = $data["type"];
+        $fact = FACTORIES[$type];
+        return $fact->CreateProduct($data);
+    }
+
+    public static function getallproducts(): array
+    {
+        $con = new dbConnect();
+        $con->connect();
+        $pre = mysqli_prepare($con->con, "SELECT * FROM products");
+        $pre->execute();
+        $result = $pre->get_result();
+        $products = [];
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+        return $products;
+    }
+
+    public static function getproductsku($sku)
+    {
+        $con = new dbConnect();
+        $con->connect();
+        $pre = mysqli_prepare($con->con, "SELECT * FROM products WHERE sku = ?");
+        $pre->bind_param("s", $sku);
+        $pre->execute();
+        $result = $pre->get_result();
+        if ($result->num_rows === 0) {
+            return 'false';
+        } else {
+            return 'true';
+        }
+    }
+
+    public static function delete($sku)
+    {
+    }
+
+    public function Addindb()
+    {
+        echo "ADDED IN DATABASE";
+        $con = new dbConnect();
+        $con->connect();
+        $pre = mysqli_prepare($con->con, "INSERT INTO products(sku, name, price, type, size, weight, height, width, length) VALUES (?,?,?,?,?,?,?,?,?)");
+        $pre->bind_param("sssssssss", $this->sku, $this->name, $this->price, $this->type, $this->size, $this->weight, $this->height, $this->width, $this->length);
+        $pre->execute();
+        echo  $this->sku;
+        echo $this->type;
+        echo $this->size;
+    }
     public function __get($name)
     {
         if (array_key_exists($name, $this->data)) {
@@ -55,3 +113,49 @@ class Book extends Product
 class Furniture extends Product
 {
 }
+/*
+DVD
+$data = [
+    'sku' => 'dasdasdsa',
+    'type' => 'DVD',
+    'nombre' => 'Producto A',
+    'precio' => 12.99,
+    'size' => 10
+
+];*/
+/*
+DVD
+$data = [
+    'sku' => 'dasdasdsa',
+    'type' => 'DVD',
+    'nombre' => 'Producto A',
+    'precio' => 12.99,
+    'size' => 10
+
+];*/
+
+
+//BOOK
+/*
+$data = [
+    'sku' => 'dasdasdsa',
+    'type' => 'BOOK',
+    'nombre' => 'Producto A',
+    'precio' => 12.99,
+    'weight' => '1 kg'
+
+];
+
+
+
+$product = Product::loadFrontendProductData($data);
+*/
+$getsku = Product::getproductsku('dasdasdsa');
+//echo $getsku;
+if ($getsku == 'true') {
+    echo 'it is true';
+} else {
+    echo 'it is false';
+}
+//$products = Product::getallproducts();
+//echo json_encode($products);
